0c32dbb81560ef77fdd3cfc77177a8c32664791a
Jonathan Strickland <djanatyn@gmail.com>
2022-11-12 13:24:26 -0500

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use miette::{Diagnostic, Result};
 use nix::fcntl;
 use nix::sys::stat::{fstat, FileStat, Mode};
 use std::collections::HashMap;
@@ -7,8 +6,10 @@ use std::io;
 use std::path::PathBuf;
 use thiserror::Error;
 
-#[derive(Debug, Diagnostic, Error)]
-enum Error {
+type Result<A> = std::result::Result<A, Error>;
+
+#[derive(Debug, Error)]
+pub enum Error {
     #[error("error reading input: {0:?}")]
     InputFailure(io::Error),
     #[error("failed to canonicalize path: {0:?}")]
@@ -22,15 +23,15 @@ enum Error {
 /// Command line arguments.
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
-struct Args {
+pub struct Args {
     /// Action subcommand.
     #[clap(subcommand)]
-    action: Action,
+    pub action: Action,
 }
 
 /// Subcommands representing command line actions.
 #[derive(Debug, clap::Subcommand)]
-enum Action {
+pub enum Action {
     /// Plan actions against filtered files.
     Plan,
     /// Execute planned actions.
@@ -38,30 +39,39 @@ enum Action {
 }
 
 /// Probe of file state.
-#[derive(Debug)]
-struct FileProbe {
+#[derive(Debug, Clone)]
+pub struct FileProbe {
     /// Initial name provided when searching for file.
-    input_name: String,
+    pub input_name: String,
     /// Absolute path, returned by canonicalize() method.
-    absolute_path: PathBuf,
+    pub absolute_path: PathBuf,
     /// FileStat returned, used for integrity checking.
-    stat: FileStat,
+    pub stat: FileStat,
     /// Mimetype returned with tree_magic::from_filepath.
-    mimetype: String,
+    pub mimetype: String,
 }
 
 /// Filter for files.
-#[derive(Debug)]
-enum Filter {
+#[derive(Debug, Clone)]
+pub enum Filter {
     /// Filter against filename using a regular expression.
     /// This matches against the canonicalized absolute path.
-    Regex(String),
+    Regex(&'static str),
     /// Filter against filenames with a mimetype.
-    Mimetype(String),
+    Mimetype(&'static str),
+}
+
+impl Filter {
+    pub fn check(&self, file: &FileProbe) -> bool {
+        match self {
+            Filter::Regex(String) => false,
+            Filter::Mimetype(mimetype) => file.mimetype == *mimetype,
+        }
+    }
 }
 
 /// Actions to take against filtered files.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum FilterAction {
     /// Move a matched file into a destination directory.
     Move { dest: String },
@@ -69,11 +79,34 @@ enum FilterAction {
     Copy { dest: String },
 }
 
+impl FilterAction {
+    fn apply(&self, target: FileProbe) {
+        match self {
+            FilterAction::Move { .. } => todo!(),
+            FilterAction::Copy { .. } => todo!(),
+        }
+    }
+}
+
 /// Predicate, containing one or more filters.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Rule {
     filters: Vec<Filter>,
     action: FilterAction,
+}
+
+impl Rule {
+    pub fn check(&self, file: &FileProbe) -> Option<Filter> {
+        for filter in &self.filters {
+            // if any filter matches,
+            if filter.check(&file) {
+                return Some(filter.clone());
+            }
+        }
+
+        // if no filters match
+        None
+    }
 }
 
 /// Probe a potential path to a file provided by the user.
@@ -109,34 +142,73 @@ fn probe_file(input_name: &str) -> Result<FileProbe> {
     })
 }
 
+/// Did the FileProbe match any Filters?
+#[derive(Debug)]
+enum Match {
+    /// Yes! We have an action to apply.
+    Yes {
+        rule: Rule,
+        filter: Filter,
+        file: FileProbe,
+    },
+    /// No, this file does not match any Rules.
+    No { file: FileProbe },
+}
+
 /// Read stdin, probe files.
 fn main() -> Result<()> {
-    // TODO: parameterize behavior on subcommand
-    let _args = Args::parse();
+    let args = Args::parse();
 
-    let _rules: Vec<Rule> = vec![Rule {
+    // define filters and actions
+    let rules: Vec<Rule> = vec![Rule {
         filters: vec![
-            Filter::Mimetype("image/png".into()),
-            Filter::Mimetype("image/jpeg".into()),
+            Filter::Mimetype("image/png"),
+            Filter::Mimetype("image/jpeg"),
+            Filter::Mimetype("image/gif"),
+            Filter::Mimetype("image/bmp"),
+            Filter::Mimetype("image/svg+xml"),
         ],
         action: FilterAction::Move {
             dest: "images".into(),
         },
     }];
 
+    // get input and probe files
     let mut probes: HashMap<String, Result<FileProbe>> = HashMap::new();
     let stdin = io::stdin();
-
     for line in stdin.lines() {
-        let text = match line {
-            Ok(text) => text,
-            Err(e) => Err(Error::InputFailure(e))?,
-        };
+        let text = line.map_err(Error::InputFailure)?;
         probes.insert(text.to_string(), probe_file(&text));
     }
 
-    dbg!(probes);
+    // check success and failure
+    let (success, failure): (Vec<_>, Vec<_>) =
+        probes.iter().partition(|(_, result)| result.is_ok());
+    println!(
+        "probed {0}/{1} files successfully",
+        success.len(),
+        probes.len()
+    );
 
+    // exit if there were any failures probing file (prompt the user to resolve)
+    if failure.len() != 0 {
+        dbg!(failure);
+    }
+
+    // start matching against our rules
+    // TODO: make this a function
+    for result in probes.values() {
+        if let Ok(file) = result {
+            for rule in &rules {
+                if let Some(filter) = rule.check(file) {
+                    // match
+                    dbg!((rule, filter, file));
+                }
+            }
+        }
+    }
+
+    println!("done");
     Ok(())
 }
 
